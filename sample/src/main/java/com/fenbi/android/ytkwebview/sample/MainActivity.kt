979284bc35f2c1d6edb9e025c9c8e6ebf773142a
Jinxin Zheng <jinxinzheng@gmail.com>
2018-12-21 14:46:52 +0800

    add ytkwebview lib and sample app

---
+++ sample/src/main/java/com/fenbi/android/ytkwebview/sample/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.fenbi.android.ytkwebview.sample
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.fenbi.android.ytkwebview.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ ytkwebview/src/main/java/com/fenbi/android/ytkwebview/YTKWebView.kt
@@ -0,0 +1,5 @@
+package com.fenbi.android.ytkwebview
+
+/**
+ * @author zheng on 12/21/18
+ */
